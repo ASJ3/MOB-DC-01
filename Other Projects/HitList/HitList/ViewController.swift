c6b6d2bd353ff16f30ad6dc609cebb856fc66771
Alexis Saint-Jean <asaintjean@gmail.com>
2014-12-29 00:09:04 -0500

    Replaced by an NSManagedObject as the source of data for the table in the view controller

--- Other Projects/HitList/HitList/ViewController.swift
+++ Other Projects/HitList/HitList/ViewController.swift
@@ -7,15 +7,85 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController, UITableViewDataSource {
-
+    
+    //functions that help add a new name to a list when you click on the "Add" button on the nav bar
     @IBAction func addName(sender: AnyObject) {
+        var alert = UIAlertController(title: "New name",
+            message: "Add a new name",
+            preferredStyle: .Alert)
+        
+        
+        /*let saveAction = UIAlertAction(title: "Save",
+            style: .Default) { (action: UIAlertAction!) -> Void in
+                
+                let textField = alert.textFields![0] as UITextField
+                self.names.append(textField.text)
+                self.tableView.reloadData()
+        }
+       */
+        
+        let saveAction = UIAlertAction(title: "Save",
+            style: .Default) { (action: UIAlertAction!) -> Void in
+                
+                let textField = alert.textFields![0] as UITextField
+                self.saveName(textField.text)
+                self.tableView.reloadData()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel",
+            style: .Default) { (action: UIAlertAction!) -> Void in
+        }
+        
+        alert.addTextFieldWithConfigurationHandler {
+            (textField: UITextField!) -> Void in
+        }
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        presentViewController(alert,
+            animated: true,
+            completion: nil)
         
     }
-    @IBOutlet weak var tableView: UITableView!
-    var names = [String]()
     
+    //function to save a new name to the core data "people" entity
+    func saveName(name: String) {
+        //1
+        let appDelegate =
+        UIApplication.sharedApplication().delegate as AppDelegate
+        
+        let managedContext = appDelegate.managedObjectContext!
+        
+        //2
+        let entity =  NSEntityDescription.entityForName("Person",
+            inManagedObjectContext:
+            managedContext)
+        
+        let person = NSManagedObject(entity: entity!,
+            insertIntoManagedObjectContext:managedContext)
+        
+        //3
+        person.setValue(name, forKey: "name")
+        
+        //4
+        var error: NSError?
+        if !managedContext.save(&error) {
+            println("Could not save \(error), \(error?.userInfo)")
+        }  
+        //5
+        people.append(person)
+    }
+    
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    //var names = [String]()
+    var people = [NSManagedObject]()
+    /*
     //functions needed for UITableViewDataSource
     func tableView(tableView: UITableView,
         numberOfRowsInSection section: Int) -> Int {
@@ -34,10 +104,31 @@ class ViewController: UIViewController, UITableViewDataSource {
             
             return cell
     }
+    */
+    
+    // new functions for UITableViewDataSource using the NSManagedObject from Core data
+    func tableView(tableView: UITableView,
+    numberOfRowsInSection section: Int) -> Int {
+    return people.count
+    }
+    
+    func tableView(tableView: UITableView,
+        cellForRowAtIndexPath
+        indexPath: NSIndexPath) -> UITableViewCell {
+            
+            let cell =
+            tableView.dequeueReusableCellWithIdentifier("Cell")
+                as UITableViewCell
+            
+            let person = people[indexPath.row]
+            cell.textLabel!.text = person.valueForKey("name") as String?
+            
+            return cell
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "\"The List\""
+        title = "\"The List 2\""
         tableView.registerClass(UITableViewCell.self,
             forCellReuseIdentifier: "Cell")
     }
