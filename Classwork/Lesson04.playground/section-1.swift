9fde65224fbbf64125a9f46c686abf2e64c8959c
Alexis Saint-Jean <asaintjean@gmail.com>
2015-01-06 21:17:26 -0500

    Lesson 4 playground: finished all the exercises

--- Classwork/Lesson04.playground/section-1.swift
+++ Classwork/Lesson04.playground/section-1.swift
@@ -35,28 +35,67 @@ if age > 16 && age < 18 {
 
 
 // TODO: Print the first fifty multiples of seven minus one (e.g. the first three multiples are 7, 14, 21. The first three multiples minus one are 6, 13, 20)
-var numberRepeats = 1
 var multiple = 0
-while numberRepeats < 51 {
-    multiple = 7 * numberRepeats - 1
+
+for i in 1...50 {
+    multiple = 7 * i - 1
     println(multiple)
-    ++numberRepeats
 }
 
 // TODO: Create a constant called number
-let numberConstant: Int = 9
+let number: Int = 9
 
 // TODO: Print whether the above number is even
-if numberConstant % 2 == 0 {
+if number % 2 == 0 {
     println("number is even")
 } else {
     println("number is uneven")
 }
 
 // TODO: The first fibonacci number is 0, the second is 1, the third is 1, the fourth is two, the fifth is 3, the sixth is 5, etc. The Xth fibonacci number is the sum of the X-1th fibonacci number and the X-2th fibonacci number. Print the 37th fibonacci number below
+var x1Num = 0
+var x2Num = 1
+
+var newNum: Int = 0
+for i in 3...37 {
+    newNum = x1Num + x2Num
+    //println("The Fibonacci number at index \(i) is: \(newNum)")
+    x1Num = x2Num
+    x2Num = newNum
+}
+println("the 37th Fibonacci number is: \(newNum)")
+
+
 
 // TODO: Print out "Hello {whatever the value of name is}, your name is {however long the string name is} characters long!. Use countElements()
+println("Hello \(Name), your name is \(countElements(Name)) characters long!")
 
 // TODO: Print the sum of one hundred random numbers. Use rand() to generate random numbers.
+var randomNum: Int32
+var addition = 0
+
+for i in 1...100 {
+    randomNum = rand()
+    addition = addition + Int(randomNum)
+    //println(randomNum)
+}
+println(addition)
 
 // Bonus TO DO: Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.
+for i in 1...100 {
+    if i % 3 == 0 && i % 5 == 0 {
+        println("FizzBuzz")
+    } else if i % 3 == 0 {
+        println("Fizz")
+    } else if i % 5 == 0 {
+        println("Buzz")
+    } else {
+       println(i)
+    }
+}
+
+
+
+
+
+
