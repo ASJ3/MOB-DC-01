949f7238426ad10057145f585dab81d74d31ee23
Alexis Saint-Jean <asaintjean@gmail.com>
2015-02-02 18:43:12 -0500

    2015/02/02 various changes

--- Classwork/Lesson 10/arrays.playground/section-1.swift
+++ Classwork/Lesson 10/arrays.playground/section-1.swift
@@ -2,6 +2,16 @@
 
 import UIKit
 
+class Animal {
+    var name = "Animal"
+    var fur = true
+    
+    init(name:String){
+        self.name = name
+    }
+    
+}
+
 var myArray = [1, 4, 10,15, 20]
 
 var secondArray: [Int] = []
@@ -38,6 +48,39 @@ for i in myArray {
 0 % 2
 1 % 2
 2 % 2
+
+var dog = Animal(name:"Picthoune")
+dog.fur
+dog.name
+
+var dog2 = Animal(name: "Bobby")
+
+var dog3 = Animal(name: "Laika")
+
+
+var emptyDictionary = [String: Int]()
+
+emptyDictionary = ["hello":5, "yes":3, "good":4]
+emptyDictionary["hello"]
+
+for (word, number) in emptyDictionary {
+    println("\(word): \(number)")
+    
+}
+
+//
+//newDictionary = [dog: 1, dog2: 2, dog3: 3]
+//
+//for (kind, number) in newDictionary {
+////    println(newDictionary[kind].name)
+//    println("\(number)")
+//}
+
+
+
+
+
+
 
 
 
