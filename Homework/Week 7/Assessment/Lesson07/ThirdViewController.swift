dec1945de0f9ef828af07dd69f4f4bcc3e0ec283
Alexis Saint-Jean <asaintjean@gmail.com>
2015-03-04 09:00:59 -0500

    Homework: working on to do three

--- Homework/Week 7/Assessment/Lesson07/ThirdViewController.swift
+++ Homework/Week 7/Assessment/Lesson07/ThirdViewController.swift
@@ -9,6 +9,42 @@
 import UIKit
 
 class ThirdViewController: UIViewController {
+    
+    @IBAction func randomButton(sender: AnyObject) {
+        println("\nsaving the text in the textView...")
+        let fileManager = NSFileManager()
+        // path variable will store the physical path to the documents folder
+        let path = fileManager.URLsForDirectory(NSSearchPathDirectory.DocumentDirectory, inDomains: NSSearchPathDomainMask.UserDomainMask) as [NSURL]
+        
+        if path.count > 0 {
+            var documentPath = path[0].relativePath! as String
+            let mySavedPath = documentPath + "/savedFile.txt"
+            
+            println("the file path is: \(mySavedPath)")
+            
+            var stringToSave = self.textView.text
+            stringToSave.writeToFile(mySavedPath, atomically: true, encoding: NSUTF32StringEncoding, error: nil)
+            
+        } else {
+            println("Could not save!")
+        }
+        
+        println("now transitioning")
+        showFourthVC()
+    }
+    
+    @IBOutlet weak var textView: UITextView!
+    @IBAction func saveAction(sender: AnyObject) {
+        println("you clicked on the Next button of the third view controller")
+
+    }
+    
+    func showFourthVC() {
+        
+        let newVC = self.storyboard?.instantiateViewControllerWithIdentifier("fourthVC") as FourthViewController
+        
+        self.presentViewController(newVC, animated: true, completion: nil)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
